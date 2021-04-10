3819adcaa737ffd698d7675d9f0b73a9b3d3db3e
uigy <uigy@pm.me>
2021-04-10 18:00:41 +0200

    create basic layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./App.scss";
 
 const App = () => {
-  return <></>;
+  return (
+    <div id="container">
+      <div className="sidebar">
+        <div className="sidebar-header"></div>
+        <div className="sidebar-content"></div>
+      </div>
+      <div id="main">
+        <div className="tasks-container"></div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
